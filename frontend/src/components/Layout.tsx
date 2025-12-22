25401eff22d93cef13e788939b7d2b0d42630ce5
Thapelo Tsotetsi <thapelotsotetsi2030@gmail.com>
2025-12-22 15:27:23 +0200

    Center file upload section.
    Signed-off-by: Thapelo Tsotetsi <thapelotsotetsi2030@gmail.com>

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -69,7 +69,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
           </div>
         </div>
       </header>
-      <main className="flex-1">{children}</main>
+      <main className="flex-1 flex items-center justify-center">{children}</main>
       <footer className="py-6 md:px-8 md:py-0">
         <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
           <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
